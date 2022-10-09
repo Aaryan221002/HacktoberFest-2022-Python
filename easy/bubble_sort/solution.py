73d41d71af7fe802dcedfe678fd2b375c1dc672a
Aaryan Sachdeva <81976330+Aaryan221002@users.noreply.github.com>
2022-10-09 19:31:32 +0530

    Update solution.py

--- easy/bubble_sort/solution.py
+++ easy/bubble_sort/solution.py
@@ -1,2 +1,7 @@
-def solution():
-    pass
+def solution(numbers):
+    for i in range(len(numbers)-1):
+        for j in range(i,len(numbers)):
+            numbers[i],numbers[j]=numbers[j],numbers[i]
+    return numbers 
+        
+   
